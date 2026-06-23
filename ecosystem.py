4289f1280b79df924589e421ec14542ef4e4ad91
swayxsoft-commits <swayxsoft@gmail.com>
2026-06-23 15:14:45 +0200

    Add files via upload

--- ecosystem.py
+++ ecosystem.py
@@ -1,18 +1,100 @@
-class Ecosystem:
-    def __init__(self):
-        self.populations = []
+"""**Файл экосистемы**
 
-    def add_population(self, population):
-        self.populations.append(population)
+Здесь описываются принципы, по которым взаимодействуют
+обьекты из organism.py"""
+
+#Импортируем классы существ
+from organism import Plant, Herbivore, Predator, Populy
+
+
+class Ecosystem:
+    """**Класс экосистемы**"""
+
+
+    def __init__(self):
+        """**Обьявление переменных**
+        
+        organisms - список всех организмов (список)"""
+
+
+
+        self.organisms = []
+
+
+    def add_organism(self, organism, population:Populy):
+        """**Функция добавления организма**
+        """
+
+        
+
+        self.organisms.append(organism)
+        population.add_organizm()
+
+
+
 
     def simulate_day(self):
-        print("Симуляция дня")
+        """**Функция симуляции взаимодействий обьектов**"""
 
-        for population in self.populations:
-            for organism in population.organisms:
-                if organism.is_alive():
-                    organism.eat(5)
-                    print(
-                        f"{organism.name}: "
-                        f"энергия {organism.energy}"
-                    )
+
+
+        #Проходимся по списку
+        for org in self.organisms:
+
+            #Если проверяемый обьект жив:
+            if org.is_alive():
+
+                #Проверяем класс обьекта
+                tp = str(type(org))
+
+                #Если класс - Predator:
+                if tp == "<class 'organism.Predator'>":
+
+                    #Проходимся снова по списку обьектов:
+                    for vict in self.organisms:
+
+                        #Проверяем класс второго обьекта
+                        vict_tp = str(type(vict))
+
+                        #Проверка условий:
+                        #Класс втрого обьекта -
+                        pos1 = vict_tp == "<class 'organism.Predator'>"
+                        #Кдасс второго обьекта -
+                        pos2 = vict_tp == "<class 'organism.Herbivore'>"
+                        #Второй обьект - это не первый
+                        pos3 = (vict != org)
+
+                        #Если (первое или второе) и третье условия:
+                        if (pos1 or pos2) and pos3:
+                            #Первый обьект пытается сьест второй
+                            org.eat(vict)
+
+                #Если класс обьекта - 
+                elif tp == "<class 'organism.Herbivore'>":
+
+                    #Проходимся снова по списку
+                    for vict in self.organisms:
+
+                        #Проверяем класс второго обьекта
+                        vict_tp = str(type(vict))
+
+                        #Класс второго обьекта - 
+                        pos1 = (vict_tp == "<class 'organism.Plant'>")
+                        #Второй обьект - это не первый
+                        pos2 = (vict != org)
+
+                        #Если первое и второе:
+                        if pos1 and pos2:
+                            #Первый обьект пытается сьест второй
+                            org.eat(vict)
+
+                #Иначе:
+                else:
+                    #Просто даем обьекту 10 энергии
+                    org.eat(10)
+
+            #Иначе - выводим сообщение:          
+            else:
+                print(f"{org.name} мёртв.")
+
+            
--- main.py
+++ main.py
@@ -1,17 +1,53 @@
-from ecosystem import Organism
-from population import Population
-from population import Ecosystem
+"""**Основной файл**
 
-eco = Ecosystem()
+Рабочее тело проекта"""
 
-rabbits = Population("Зайцы")
 
-rabbit1 = Organism("Заяц 1", 20)
-rabbit2 = Organism("Заяц 2", 15)
+#Импортируем модули
+from ecosystem import Ecosystem
+from organism import Plant, Predator, Herbivore, Populy
 
-rabbits.add_organism(rabbit1)
-rabbits.add_organism(rabbit2)
 
-eco.add_population(rabbits)
 
-eco.simulate_day()
+def main():
+    """**Основная функция**
+    
+    Рабочее тело проекта"""
+
+
+    eco = Ecosystem()  #Экосистема
+    plants = Populy()  #Популяция растений
+    preds = Populy()  #Популяция хищников
+    herbs = Populy()  #Популяция травоядных
+
+    #Заяц - травоядное
+    rabbit = Herbivore("Заяц", 20, 15)  
+    #Лиса - хищник
+    fox = Predator("Лиса", 30, 20)  
+    #Клубника - растение
+    strawberry = Plant("Клубника", 50)  
+
+
+    #Добавляем существ в экосистему и соответствующие популяции
+    eco.add_organism(strawberry, plants) 
+    eco.add_organism(rabbit, herbs)
+    eco.add_organism(fox, preds)
+
+
+
+
+    #Симулируем день
+    eco.simulate_day()
+
+
+    #Выводим количество существ в популяциях после дня:
+    print(f"Растений: {plants.populy}")
+    print(f"Хищников: {preds.populy}")
+    print(f"Травоядных: {herbs.populy}")
+
+
+
+
+#Запускаем основную функцию:
+if __name__ == "__main__":
+    main()
