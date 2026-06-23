a9ff458aaf18c9b11d8e50f7b67560ea9fb4ebe9
swayxsoft-commits <swayxsoft@gmail.com>
2026-06-23 15:15:49 +0200

    Delete population.py

--- population.py
+++
@@ -1,7 +0,0 @@
-class Population:
-    def __init__(self, species):
-        self.species = species
-        self.organisms = []
-
-    def add_organism(self, organism):
-        self.organisms.append(organism)
